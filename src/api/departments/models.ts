21c3c4b9dcb326ecd2771b6831eb65e496048195
Yunfan Yang <cloudy.young@outlook.com>
2024-04-29 16:57:04 -0600

    Update controller.ts

---
+++ src/api/departments/models.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+const { Schema, connection } = mongoose;
+
+const CatalogDepartmentSchema = new Schema({
+  id: {
+    type: String,
+    required: true
+  },
+  display_name: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+}, {
+  timestamps: true
+});
+
+const catalog = connection.useDb('catalog');
+const CatalogDepartment = catalog.model('Department', CatalogDepartmentSchema)
+export { CatalogDepartment }
