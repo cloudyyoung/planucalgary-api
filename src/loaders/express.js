e06a457d15b648538f2e5551a2a52af2b9a34062
Yunfan Yang <cloudy.young@outlook.com>
2024-04-29 01:21:04 -0600

    Update express.js

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,4 +1,3 @@
-import express from 'express'
 import cors from 'cors';
 import compression from 'compression';
 import morgan from 'morgan';
@@ -21,29 +20,10 @@ export default (app) => {
   app.use(morgan('dev'));
   app.use(helmet());
   app.use(compression());
-  app.use(express.static('public'));
   app.disable('x-powered-by');
   app.disable('etag');
 
   app.get('/', (_req, res) => {
     return res.status(200).json({ message: "ok" }).end();
-  });
-
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.header('Content-Security-Policy-Report-Only', 'default-src: https:');
-    if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', 'PUT POST PATCH DELETE GET');
-      return res.status(200).json({});
-    }
-    next();
-  });
-
-  app.use((_req, _res, next) => {
-    const error = new Error('Endpoint could not find!');
-    error.status = 404;
-    next(error);
   });
 }
