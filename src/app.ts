64060cbe3a8b4cea5641b2d6a9bea0782e25c128
lybned <nedliu2001@gmail.com>
2024-04-29 17:52:22 -0600

    Removed unused code in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,6 @@
 import express from 'express';
 import { PORT } from './config';
 import loader from './loaders/index';
-import mongoose from 'mongoose';
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
 
 const app = express();
 
