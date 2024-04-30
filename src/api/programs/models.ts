7c0da0b7ef6998f65c057dd05035092d328b02ad
Yunfan Yang <cloudy.young@outlook.com>
2024-04-30 02:06:58 -0600

    Move to types

--- src/api/programs/models.ts
+++ src/api/programs/models.ts
@@ -1,30 +1,8 @@
 import mongoose from 'mongoose';
 import { RequisitesSchema } from '../requisites/models';
-import { RequisitesProps } from '../requisites/types';
+import { CatalogProgramProps } from './types';
 
 const { Schema, connection } = mongoose;
-
-interface CatalogProgramProps {
-  active: boolean;
-  admission_info: string;
-  career: string;
-  code: string;
-  coursedog_id: string;
-  degree_designation_code: string;
-  degree_designation_name: string;
-  departments: string[];
-  display_name: string;
-  general_info: string;
-  long_name: string;
-  name: string;
-  program_group_id: string;
-  requisites: RequisitesProps;
-  start_term: Map<string, any>;
-  transcript_description: string;
-  transcript_level: string;
-  type: string;
-  version: number;
-}
 
 const CatalogProgramSchema = new Schema<CatalogProgramProps>({
   active: {
---
+++ src/api/programs/types.ts
@@ -0,0 +1,25 @@
+import { RequisitesProps } from '../requisites/types';
+
+interface CatalogProgramProps {
+  active: boolean;
+  admission_info: string;
+  career: string;
+  code: string;
+  coursedog_id: string;
+  degree_designation_code: string;
+  degree_designation_name: string;
+  departments: string[];
+  display_name: string;
+  general_info: string;
+  long_name: string;
+  name: string;
+  program_group_id: string;
+  requisites: RequisitesProps;
+  start_term: Map<string, any>;
+  transcript_description: string;
+  transcript_level: string;
+  type: string;
+  version: number;
+}
+
+export type { CatalogProgramProps };
