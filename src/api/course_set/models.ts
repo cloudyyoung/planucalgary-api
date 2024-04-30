30783b5f7a379c8e9f03bf12ad6a93472d35c0e8
Yunfan Yang <cloudy.young@outlook.com>
2024-04-29 21:05:37 -0600

    Update models.ts

--- src/api/course_set/models.ts
+++ src/api/course_set/models.ts
@@ -3,15 +3,43 @@ import mongoose from 'mongoose';
 const { Schema, connection } = mongoose;
 
 interface CatalogCourseSetProps {
-
+  course_list: string[];
+  description: string | null;
+  id: string;
+  name: string;
+  structure: object;
+  type: "static" | "dynamic";
 }
 
 const CatalogCourseSetSchema = new Schema<CatalogCourseSetProps>({
-
+  course_list: {
+    type: [String],
+    required: true
+  },
+  description: {
+    type: String,
+  },
+  id: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  structure: {
+    type: Map,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true,
+    enum: ["static", "dynamic"]
+  }
 }, {
   timestamps: true
 });
 
 const catalog = connection.useDb('catalog');
-const CatalogCourseSet = catalog.model('Program', CatalogCourseSetSchema)
+const CatalogCourseSet = catalog.model('CourseSet', CatalogCourseSetSchema, 'course_sets');
 export { CatalogCourseSet }
