41c7cc3c5f45f5454fdb4aad95f2d8321fcf197b
Yunfan Yang <cloudy.young@outlook.com>
2024-04-30 03:35:08 -0600

    Update engine.ts

--- src/api/requisites/engine.ts
+++ src/api/requisites/engine.ts
@@ -1,5 +1,6 @@
 import { plainToClass } from 'class-transformer';
 import { Requisites } from "./classes/requisites";
+import { StructureCondition } from './classes/structure_condition';
 
 class RequisitesEngine {
   private requisites: Requisites
@@ -20,7 +21,13 @@ class RequisitesEngine {
 }
 
 class StructureConditionEngine {
+  private structureCondition: StructureCondition
+  private facts: any
 
+  constructor(structureCondition: any, facts: any) {
+    this.structureCondition = plainToClass(StructureCondition, structureCondition)
+    this.facts = facts
+  }
 }
 
 export default RequisitesEngine;
