de9432d5cfb19f67c589a8e1e8f3023b50b008df
Yunfan Yang <cloudy.young@outlook.com>
2024-04-30 04:02:32 -0600

    Create requisite set model

---
+++ src/api/requisite_set/model.ts
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+const { Schema, connection } = mongoose;
+
+const CatalogRequisiteSetSchema = new Schema<RequisiteSetProps>({
+  description: {
+    type: String,
+  },
+  id: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  requisite_set_group_id: {
+    type: String,
+    required: true
+  },
+  requisites: {
+    type: Map,
+    required: true
+  },
+  version: {
+    type: Number,
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+const catalog = connection.useDb('catalog');
+const CatalogRequisiteSet = catalog.model('RequisiteSet', CatalogRequisiteSetSchema, 'requisite_sets');
+export { CatalogRequisiteSet }
---
+++ src/api/requisite_set/types.ts
@@ -0,0 +1,8 @@
+interface RequisiteSetProps {
+  id: string;
+  name: string;
+  description: string;
+  requisite_set_group_id: string;
+  requisites: object;
+  version: number;
+}
