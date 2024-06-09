827232781beb28d96d8e440c948bc803326ebc87
Sipeng He <hsp8412@hotmail.com>
2024-06-08 18:44:38 -0600

    fix bugs for jsonLogic custom operations

--- src/jsonLogic/operators.ts
+++ src/jsonLogic/operators.ts
@@ -1,11 +1,11 @@
 import { CourseDocument } from "../api/catalog_courses/types"
 
-export function courses(data: any, from: CourseDocument[], required: number): boolean {
+export function courses(from: CourseDocument[], required: number): boolean {
   const count = from.length
   return count >= required
 }
 
-export function from(data: any, ...courses: (CourseDocument | null)[]) {
+export function from(...courses: (CourseDocument | null)[]) {
   return courses.filter(Boolean) as CourseDocument[]
 }
 
@@ -30,18 +30,18 @@ export function units(data: { courses?: CourseDocument[] }, from: CourseDocument
   return unitsCount >= required
 }
 
-export function consent(data: any, ...consenter: any[]) {
+export function consent(...consenter: string[]) {
   console.log("Consent of: " + (consenter.length > 0 ? consenter.join(", ") : "None"))
   return true
 }
 
-export function admission(data: any, ...admissionTo: any[]) {
+export function admission(...admissionTo: string[]) {
   console.log("Admission to: " + (admissionTo.length > 0 ? admissionTo.join(", ") : "None"))
   return true
 }
 
 // for antireqs
-export function and_to_or(data: any, ...args: any) {
+export function and_to_or(...args: any) {
   args.forEach((arg: any) => {
     if (arg) {
       return true
