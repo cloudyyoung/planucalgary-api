b1353f0802b88f867375b36da11a20f2aedbb6b4
Yunfan Yang <cloudy.young@outlook.com>
2024-11-25 01:03:21 -0700

    Update controllers.ts

--- src/api/courses/controllers.ts
+++ src/api/courses/controllers.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express"
 
 export const createCourse = async (req: Request, res: Response) => {
-  console.log("createCourse")
   const course = await req.prisma.course.create({
     data: req.body,
   })
