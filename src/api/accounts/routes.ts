e73b5fbcd98ad5f1ceb8d799279f4c2152730017
Yunfan Yang <cloudy.young@outlook.com>
2024-11-25 00:57:47 -0700

    Add zod middleware

--- src/api/accounts/routes.ts
+++ src/api/accounts/routes.ts
@@ -2,11 +2,13 @@ import { Router } from "express"
 
 import { signin } from "./controllers/signin"
 import { signup } from "./controllers/signup"
+import { zod } from "../../middlewares"
+import { SignInInputSchema, SignUpInputSchema } from "./validators"
 
 const router = Router()
 
-router.post("/signin", signin)
-router.post("/signup", signup)
+router.post("/signin", zod({ body: SignInInputSchema }), signin)
+router.post("/signup", zod({ body: SignUpInputSchema }), signup)
 
 export default router
 export { router }
---
+++ src/middlewares/zod.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express"
+import { SafeParseSuccess, ZodSchema } from "zod"
+
+export interface ZodmiddlewareOptions {
+  body?: ZodSchema
+}
+
+export const zod = (options: ZodmiddlewareOptions) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    if (options.body) {
+      const parsed = options.body.safeParse(req.body)
+      if (!parsed.success) {
+        return res.status(400).json({ error: parsed.error.errors }).end()
+      }
+      req.body = parsed.data
+      req.zod = parsed
+    }
+    next()
+  }
+}
+
+declare module "express-serve-static-core" {
+  interface Request {
+    zod?: SafeParseSuccess<unknown>
+  }
+}
