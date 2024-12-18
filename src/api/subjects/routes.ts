d5561f6cdf28ea0a8b0c67fb88194897ff456118
Yunfan Yang <cloudy.young@outlook.com>
2024-12-18 02:47:57 -0700

    Fix schema

--- src/api/subjects/routes.ts
+++ src/api/subjects/routes.ts
@@ -3,13 +3,13 @@ import { Router } from "express"
 import { createSubject, getSubject, listSubjects, updateSubject } from "./controllers"
 import { admin } from "../../middlewares/admin"
 import { IdInputSchema, zod } from "../../middlewares"
-import { FacultyCreateSchema, FacultyUpdateSchema } from "../../zod"
+import { SubjectCreateSchema, SubjectUpdateSchema } from "../../zod"
 
 const router = Router()
 router.get("/", listSubjects)
 router.get("/:id", zod({ params: IdInputSchema }), getSubject)
-router.post("/", admin(), zod({ body: FacultyCreateSchema }), createSubject)
-router.put("/:id", admin(), zod({ params: IdInputSchema, body: FacultyUpdateSchema }), updateSubject)
+router.post("/", admin(), zod({ body: SubjectCreateSchema }), createSubject)
+router.put("/:id", admin(), zod({ params: IdInputSchema, body: SubjectUpdateSchema }), updateSubject)
 
 export default router
 export { router }
