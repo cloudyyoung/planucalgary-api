f21040e4e988d64e55a17ba4c13ce00dfe954105
Yunfan Yang <cloudy.young@outlook.com>
2025-02-28 21:35:14 -0700

    Update schema.ts

--- src/jsonlogic/schema.ts
+++ src/jsonlogic/schema.ts
@@ -1,216 +1,6 @@
-import Ajv, { Schema } from "ajv"
+import Ajv from "ajv"
 
-const and: Schema = {
-  type: "object",
-  description: "Logic operator of a relationship A and B",
-  required: ["and"],
-  additionalProperties: false,
-  properties: {
-    and: {
-      type: "array",
-      items: {
-        anyOf: [
-          { $ref: "#/definitions/course" },
-          { $ref: "#/definitions/or" },
-          { $ref: "#/definitions/not" },
-          { $ref: "#/definitions/units" },
-          { $ref: "#/definitions/consent" },
-          { $ref: "#/definitions/admission" },
-          { $ref: "#/definitions/year" },
-        ],
-      },
-    },
-  },
-}
-
-const or: Schema = {
-  type: "object",
-  description: "Logic operator of a relationship A or B",
-  required: ["or"],
-  additionalProperties: false,
-  properties: {
-    or: {
-      type: "array",
-      items: {
-        anyOf: [
-          { $ref: "#/definitions/course" },
-          { $ref: "#/definitions/and" },
-          { $ref: "#/definitions/not" },
-          { $ref: "#/definitions/units" },
-          { $ref: "#/definitions/consent" },
-          { $ref: "#/definitions/admission" },
-          { $ref: "#/definitions/year" },
-        ],
-      },
-    },
-  },
-}
-
-const not: Schema = {
-  type: "object",
-  description: "Logic operator of a relationship not A",
-  required: ["not"],
-  additionalProperties: false,
-  properties: {
-    not: {
-      anyOf: [
-        { $ref: "#/definitions/course" },
-        { $ref: "#/definitions/and" },
-        { $ref: "#/definitions/not" },
-        { $ref: "#/definitions/units" },
-        { $ref: "#/definitions/consent" },
-        { $ref: "#/definitions/admission" },
-        { $ref: "#/definitions/year" },
-      ],
-    },
-  },
-}
-
-const units: Schema = {
-  type: "object",
-  required: ["units"],
-  additionalProperties: false,
-  properties: {
-    units: {
-      type: "integer",
-    },
-    from: {
-      anyOf: [
-        {
-          type: "array",
-          items: {
-            anyOf: [{ $ref: "#/definitions/course" }, { type: "null" }],
-          },
-        },
-        { type: "null" },
-      ],
-    },
-    exclude: {
-      anyOf: [
-        {
-          type: "array",
-          items: {
-            anyOf: [{ $ref: "#/definitions/course" }, { type: "null" }],
-          },
-        },
-        { type: "null" },
-      ],
-    },
-    field: {
-      oneOf: [{ type: "string" }, { type: "null" }],
-    },
-    level: {
-      oneOf: [{ $ref: "#/definitions/level" }, { type: "null" }],
-    },
-    subject: {
-      oneOf: [{ $ref: "#/definitions/subject" }, { type: "null" }],
-    },
-    faculty: {
-      oneOf: [{ $ref: "#/definitions/faculty" }, { type: "null" }],
-    },
-    department: {
-      oneOf: [{ $ref: "#/definitions/department" }, { type: "null" }],
-    },
-  },
-}
-
-const consent: Schema = {
-  type: "object",
-  required: ["consent"],
-  additionalProperties: false,
-  properties: {
-    consent: {
-      oneOf: [{ $ref: "#/definitions/faculty_object" }, { $ref: "#/definitions/department_object" }],
-    },
-  },
-}
-
-const admission: Schema = {
-  type: "object",
-  required: ["admission"],
-  additionalProperties: false,
-  properties: {
-    admission: {
-      type: "object",
-      oneOf: [
-        { $ref: "#/definitions/faculty_object" },
-        { $ref: "#/definitions/department_object" },
-        { $ref: "#/definitions/program_object" },
-      ],
-    },
-  },
-}
-
-const faculty_object: Schema = {
-  type: "object",
-  required: ["faculty"],
-  additionalProperties: false,
-  properties: {
-    faculty: { $ref: "#/definitions/faculty" },
-  },
-}
-
-const department_object: Schema = {
-  type: "object",
-  required: ["department"],
-  additionalProperties: false,
-  properties: {
-    department: { $ref: "#/definitions/department" },
-  },
-}
-
-const program_object: Schema = {
-  type: "object",
-  required: ["program"],
-  additionalProperties: false,
-  properties: {
-    program: { $ref: "#/definitions/program" },
-  },
-}
-
-const year: Schema = {
-  type: "object",
-  required: ["year"],
-  additionalProperties: false,
-  properties: {
-    year: {
-      type: "string",
-      enum: ["first", "second", "third", "fourth", "fifth"],
-    },
-  },
-}
-
-// Primitives
-const course: Schema = {
-  type: "string",
-  pattern: "^[A-Z]{3,4}[0-9]{2,3}(-[0-9])?(.[0-9]{2})?[AB]?$",
-}
-
-const level: Schema = {
-  type: "string",
-  pattern: "^[0-9]{2,3}[+]?$",
-}
-
-const subject: Schema = {
-  type: "string",
-  pattern: "^[A-Z]{3,4}$",
-}
-
-const faculty: Schema = {
-  type: "string",
-  pattern: "^[A-Z]{2}$",
-}
-
-const department: Schema = {
-  type: "string",
-  pattern: "^[A-Z]{3,4}$",
-}
-
-const program: Schema = {
-  type: "string",
-}
-
-export const schema: Schema = {
+export const schema = {
   anyOf: [
     // Primitives
     // Course is the only primitive that can be at the root level, returns True if the course exists in data
@@ -225,28 +15,191 @@ export const schema: Schema = {
     { $ref: "#/definitions/admission" },
     { $ref: "#/definitions/year" },
   ],
+
   definitions: {
     // Primitives
-    course,
-    level,
-    subject,
-    faculty,
-    department,
-    program,
+    course: {
+      type: "string",
+      pattern: "^[A-Z]{3,4}[0-9]{2,3}(-[0-9])?(.[0-9]{2})?[AB]?$",
+    },
+    level: {
+      type: "string",
+      pattern: "^[0-9]{2,3}[+]?$",
+    },
+    subject: {
+      type: "string",
+      pattern: "^[A-Z]{3,4}$",
+    },
+    faculty: {
+      type: "string",
+      pattern: "^[A-Z]{2}$",
+    },
+    department: {
+      type: "string",
+      pattern: "^[A-Z]{3,4}$",
+    },
+    program: {
+      type: "string",
+    },
+    year_str: {
+      type: "string",
+      enum: ["first", "second", "third", "fourth", "fifth"],
+    },
 
     // Boolean operators
-    and,
-    or,
-    not,
-    units,
-    consent,
-    admission,
-    year,
+    and: {
+      properties: {
+        and: {
+          type: "array",
+          items: {
+            anyOf: [
+              { $ref: "#/definitions/course" },
+              { $ref: "#/definitions/or" },
+              { $ref: "#/definitions/not" },
+              { $ref: "#/definitions/units" },
+              { $ref: "#/definitions/consent" },
+              { $ref: "#/definitions/admission" },
+              { $ref: "#/definitions/year" },
+            ],
+          },
+        },
+      },
+    },
+    or: {
+      properties: {
+        or: {
+          type: "array",
+          items: {
+            anyOf: [
+              { $ref: "#/definitions/course" },
+              { $ref: "#/definitions/and" },
+              { $ref: "#/definitions/not" },
+              { $ref: "#/definitions/units" },
+              { $ref: "#/definitions/consent" },
+              { $ref: "#/definitions/admission" },
+              { $ref: "#/definitions/year" },
+            ],
+          },
+        },
+      },
+    },
+    not: {
+      properties: {
+        not: {
+          anyOf: [
+            { $ref: "#/definitions/course" },
+            { $ref: "#/definitions/and" },
+            { $ref: "#/definitions/or" },
+            { $ref: "#/definitions/units" },
+            { $ref: "#/definitions/consent" },
+            { $ref: "#/definitions/admission" },
+            { $ref: "#/definitions/year" },
+          ],
+        },
+      },
+    },
+    units: {
+      type: "object",
+      required: ["units"],
+      additionalProperties: false,
+      properties: {
+        units: {
+          type: "integer",
+        },
+        from: {
+          type: "array",
+          nullable: true,
+          items: { $ref: "#/definitions/course" },
+        },
+        exclude: {
+          type: "array",
+          nullable: true,
+          items: { $ref: "#/definitions/course" },
+        },
+        field: {
+          type: "string",
+          nullable: true,
+        },
+        level: {
+          type: "object",
+          nullable: true,
+          $ref: "#/definitions/level",
+        },
+        subject: {
+          type: "object",
+          nullable: true,
+          $ref: "#/definitions/subject",
+        },
+        faculty: {
+          type: "object",
+          nullable: true,
+          $ref: "#/definitions/faculty",
+        },
+        department: {
+          type: "object",
+          nullable: true,
+          $ref: "#/definitions/department",
+        },
+      },
+    },
+    consent: {
+      type: "object",
+      required: ["consent"],
+      additionalProperties: false,
+      properties: {
+        consent: {
+          oneOf: [{ $ref: "#/definitions/faculty_object" }, { $ref: "#/definitions/department_object" }],
+        },
+      },
+    },
+    admission: {
+      type: "object",
+      required: ["admission"],
+      additionalProperties: false,
+      properties: {
+        admission: {
+          oneOf: [
+            { $ref: "#/definitions/faculty_object" },
+            { $ref: "#/definitions/department_object" },
+            { $ref: "#/definitions/program_object" },
+          ],
+        },
+      },
+    },
+    year: {
+      type: "object",
+      required: ["year"],
+      additionalProperties: false,
+      properties: {
+        year: { $ref: "#/definitions/year_str" },
+      },
+    },
 
     // Objects
-    faculty_object,
-    department_object,
-    program_object,
+    faculty_object: {
+      type: "object",
+      required: ["faculty"],
+      additionalProperties: false,
+      properties: {
+        faculty: { $ref: "#/definitions/faculty" },
+      },
+    },
+    department_object: {
+      type: "object",
+      required: ["department"],
+      additionalProperties: false,
+      properties: {
+        department: { $ref: "#/definitions/department" },
+      },
+    },
+    program_object: {
+      type: "object",
+      required: ["program"],
+      additionalProperties: false,
+      properties: {
+        program: { $ref: "#/definitions/program" },
+      },
+    },
   },
 }
 
