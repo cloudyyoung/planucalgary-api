2befd5ae69a0ef5250005b6f960fe32fc92b48b7
Yunfan Yang <cloudy.young@outlook.com>
2025-03-02 00:10:18 -0700

    Add requisite_json validators

---
+++ src/jsonlogic/requisite_json.test.ts
@@ -0,0 +1,71 @@
+import { RequisiteJsonError, validate } from "./requisite_json"
+
+describe("validator", () => {
+  it("validate course, truthy", async () => {
+    const json = "CPSC217"
+    const result = await validate(json, false)
+    expect(result).toBe(true)
+  })
+
+  it("validate course, falsy", async () => {
+    const json = "ABCD1237"
+    const result = await validate(json, false)
+    expect(result).toBe(false)
+  })
+
+  it("validate and with two courses, truthy", async () => {
+    const json = { and: ["CPSC217", "CPSC231"] }
+    const result = await validate(json, false)
+    expect(result).toBe(true)
+  })
+
+  it("validate and with two courses, truthy", async () => {
+    const json = { and: ["ABCD222", "TTTT222"] }
+    const result = await validate(json, false)
+    expect(result).toBe(false)
+  })
+
+  it("validate units with from, truthy", async () => {
+    const json = {
+      from: ["ENGG213", "COMS363", "SGMA217"],
+      units: 3,
+    }
+    const result = await validate(json, false)
+    expect(result).toBe(true)
+  })
+
+  it("validate and with two arguments of units, falsy", async () => {
+    const json = {
+      and: [
+        {
+          from: ["CPSC251", "MATH271", "MATH273"],
+          units: 3,
+        },
+        {
+          from: ["CPSC219", "CPSC233", "CPSC235", "ENCM335", "ENCM339", "ENSF337"],
+          units: 3,
+        },
+      ],
+    }
+
+    const t = async () => await validate(json, false)
+    await expect(t()).rejects.toThrow(RequisiteJsonError)
+  })
+
+  it("validate and with two arguments of units + another or, truthy", async () => {
+    const json = {
+      and: [
+        {
+          from: ["DRAM210", "DRAM223", "DRAM225"],
+          units: 12,
+        },
+        {
+          or: ["DRAM240", "DRAM243"],
+        },
+      ],
+    }
+
+    const result = await validate(json, false)
+    expect(result).toBe(true)
+  })
+})
