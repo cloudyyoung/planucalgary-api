54750dab7e6f8e6aa352f030873559ccdd0d2d42
Yunfan Yang <cloudy.young@outlook.com>
2025-03-02 02:28:55 -0700

    Fix tests

--- src/jsonlogic/requisite_json.test.ts
+++ src/jsonlogic/requisite_json.test.ts
@@ -105,4 +105,175 @@ describe("validator", () => {
     const result = validate(json, false)
     expect(result.valid).toBe(true)
   })
+
+  it("valid a really simple json: course, truthy", async () => {
+    const json = "CPSC217"
+    const result = validate(json)
+    expect(result.valid).toBe(true)
+  })
+
+  it("valid a simple json: course, falsy", async () => {
+    const json = "ABCD123"
+    const result = validate(json)
+    expect(result.valid).toBe(false)
+  })
+
+  it("valid a simple json: and with two courses, truthy", async () => {
+    const json = { and: ["ACCT445", "ACCT357"] }
+    const result = validate(json)
+    expect(result.valid).toBe(true)
+  })
+
+  it("valid a simple json: and with two courses, falsy", async () => {
+    const json = { and: ["ABCD123", "CDEF456"] }
+    const result = validate(json)
+    expect(result.valid).toBe(false)
+  })
+
+  it("valid a simpler json, and with units + or, truthy", async () => {
+    const json = {
+      and: [
+        {
+          units: 24,
+          subject: "CPSC",
+        },
+        {
+          or: ["ACCT425", "ACCT357"],
+        },
+      ],
+    }
+    const result = validate(json)
+    expect(result.valid).toBe(true)
+  })
+
+  it("valid a simpler json: and with units + units + or, falsy", async () => {
+    const json = {
+      and: [
+        {
+          units: 24,
+          subject: "ABCD",
+        },
+        {
+          units: 24,
+          department: "XXXXXX",
+        },
+        {
+          or: [true, "ACCT301"],
+        },
+      ],
+    }
+    const result = validate(json)
+    expect(result.valid).toBe(false)
+  })
+
+  it("valid a complex json: and with units + consent, truthy", async () => {
+    const json = {
+      and: [
+        {
+          units: 15,
+          subject: "ART",
+          department: "ART",
+        },
+        {
+          consent: {
+            faculty: "SC",
+          },
+        },
+      ],
+    }
+    const result = validate(json)
+    expect(result.valid).toBe(true)
+  })
+
+  it("valid a year json", async () => {
+    const json = {
+      year: "fourth",
+    }
+    const result = validate(json)
+    expect(result.valid).toBe(true)
+  })
+
+  it("valid a complex json: and with course + admission + consent, truthy", async () => {
+    const json = {
+      and: [
+        "KNES260",
+        {
+          admission: {
+            faculty: "KN",
+          },
+        },
+        {
+          consent: {
+            faculty: "KN",
+          },
+        },
+      ],
+    }
+    const result = validate(json)
+    expect(result.valid).toBe(true)
+  })
+
+  it("valid a complex json: and with units + consent, truthy", async () => {
+    const json = {
+      and: [
+        {
+          level: "300+",
+          units: 18,
+          subject: "ARHI",
+        },
+        {
+          consent: {
+            department: "ART",
+          },
+        },
+      ],
+    }
+    const result = validate(json)
+    expect(result.valid).toBe(true)
+  })
+
+  it("valid a complex json: and with units + nested or + year, truthy", async () => {
+    const json = {
+      and: [
+        {
+          from: ["MATH249", "MATH265", "MATH275"],
+          units: 3,
+        },
+        {
+          or: [
+            {
+              units: 3,
+              faculty: "SC",
+            },
+            {
+              units: 3,
+              faculty: "EN",
+            },
+          ],
+        },
+        {
+          year: "third",
+        },
+      ],
+    }
+    const result = validate(json)
+    expect(result.valid).toBe(true)
+  })
+
+  it("valid a complex json: or with untracked course, falsy", async () => {
+    const json = {
+      or: ["PLAN602", "Environmental Design Planning 602"],
+    }
+    const result = validate(json)
+    expect(result.valid).toBe(false)
+  })
+
+  it("valid a complex json: units with level, falsy", async () => {
+    const json = {
+      units: 3,
+      level: "AA",
+    }
+    const result = validate(json)
+    expect(result.valid).toBe(false)
+  })
 })
