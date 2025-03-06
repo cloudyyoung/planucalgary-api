79480af4d9304752814164d6ae95d05a90d52e1b
Yunfan Yang <cloudy.young@outlook.com>
2025-03-06 00:28:28 -0700

    Update routes.ts

--- src/api/programs/routes.ts
+++ src/api/programs/routes.ts
@@ -3,14 +3,19 @@ import { Router } from "express"
 import { createProgram, deleteProgram, getProgram, listPrograms, updateProgram } from "./controllers"
 import { admin } from "../../middlewares/admin"
 import { IdInputSchema, zod } from "../../middlewares"
-import { ProgramListSchema } from "./validators"
+import { ProgramCreateRelationsSchema, ProgramListSchema } from "./validators"
 import { ProgramCreateSchema, ProgramUpdateSchema } from "../../zod"
 
 const router = Router()
 router.get("/", zod({ query: ProgramListSchema }), listPrograms)
 router.get("/:id", zod({ params: IdInputSchema }), getProgram)
-router.post("/", admin(), zod({ body: ProgramCreateSchema }), createProgram)
-router.put("/:id", admin(), zod({ params: IdInputSchema, body: ProgramUpdateSchema }), updateProgram)
+router.post("/", admin(), zod({ body: ProgramCreateSchema.merge(ProgramCreateRelationsSchema) }), createProgram)
+router.put(
+  "/:id",
+  admin(),
+  zod({ params: IdInputSchema, body: ProgramUpdateSchema.merge(ProgramCreateRelationsSchema) }),
+  updateProgram,
+)
 router.delete("/:id", admin(), zod({ params: IdInputSchema }), deleteProgram)
 
 export default router
