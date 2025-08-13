56dd27075b8d7d09d4c61bd75a707b62e36c7927
Yunfan Yang <cloudy.young@outlook.com>
2025-08-13 04:18:09 -0600

    Update requisite_json_logic.test.ts

--- src/jsonlogic/requisite_json_logic.test.ts
+++ src/jsonlogic/requisite_json_logic.test.ts
@@ -36,13 +36,317 @@ describe('RequisiteJsonLogic', () => {
         }
       ]
     }
-    const isLogic = RequisiteJsonLogic.is_rule(logic)
-    expect(isLogic).toBeTruthy()
+    const isRule = RequisiteJsonLogic.is_rule(logic)
+    expect(isRule).toBeTruthy()
   })
-
   it("validate course, truthy", () => {
     const json = "CPSC217"
-    const result = RequisiteJsonLogic.is_rule(json)
-    expect(result).toBe(true)
+    const isRule = RequisiteJsonLogic.is_rule(json)
+    expect(isRule).toBe(true)
+  })
+
+  it("validate course, falsy", () => {
+    const json = "ABCD1237"
+    const isRule = RequisiteJsonLogic.is_rule(json)
+    expect(isRule).toBe(false)
+  })
+
+  it("validate and with two courses, truthy", () => {
+    const json = { and: ["CPSC217", "CPSC231"] }
+    const isRule = RequisiteJsonLogic.is_rule(json)
+    expect(isRule).toBe(true)
+  })
+
+  it("validate and with two courses, truthy", () => {
+    const json = { and: ["ABCD222", "TTTT222"] }
+    const isRule = RequisiteJsonLogic.is_rule(json)
+    expect(isRule).toBe(false)
+  })
+
+  it("validate units with from, truthy", () => {
+    const json = {
+      from: ["ENGG213", "COMS363", "SGMA217"],
+      units: 3,
+    }
+    const isRule = RequisiteJsonLogic.is_rule(json)
+    expect(isRule).toBe(true)
+  })
+
+  it("validate and with two arguments of units, falsy", () => {
+    const json = {
+      and: [
+        {
+          from: ["CPSC251", "MATH271", "MATH273", "MATH999"],
+          units: 3,
+        },
+        {
+          from: ["CPSC219", "CPSC233", "CPSC235", "ENCM335", "ENCM339", "ENSF337"],
+          units: 3,
+        },
+      ],
+    }
+
+    const isRule = RequisiteJsonLogic.is_rule(json)
+    expect(isRule).toBe(false)
+  })
+
+  it("validate and with two arguments of units + another or, truthy", () => {
+    const json = {
+      and: [
+        {
+          from: ["DRAM210", "DRAM223", "DRAM225"],
+          units: 12,
+        },
+        {
+          or: ["DRAM242", "DRAM243"],
+        },
+      ],
+    }
+
+    const isRule = RequisiteJsonLogic.is_rule(json)
+    expect(isRule).toBe(true)
+  })
+
+  it("validate and with only one argument, false", () => {
+    const json = {
+      and: [
+        {
+          or: ["DRAM243"],
+        },
+      ],
+    }
+
+    const isRule = RequisiteJsonLogic.is_rule(json)
+    expect(isRule).toBe(false)
+  })
+
+  it("validate year, truthy", () => {
+    const json = {
+      and: [
+        {
+          year: "third",
+        },
+        {
+          consent: {
+            faculty: "AR",
+          },
+        },
+      ],
+    }
+
+    const isRule = RequisiteJsonLogic.is_rule(json)
+    expect(isRule).toBe(true)
+  })
+
+  it("valid a really simple json: course, truthy", () => {
+    const json = "CPSC217"
+    const isRule = RequisiteJsonLogic.is_rule(json)
+    expect(isRule).toBe(true)
+  })
+
+  it("valid a simple json: course, falsy", () => {
+    const json = "ABCD123"
+    const isRule = RequisiteJsonLogic.is_rule(json)
+    expect(isRule).toBe(false)
+  })
+
+  it("valid a simple json: and with two courses, truthy", () => {
+    const json = { and: ["ACCT445", "ACCT357"] }
+    const isRule = RequisiteJsonLogic.is_rule(json)
+    expect(isRule).toBe(true)
+  })
+
+  it("valid a simple json: and with two courses, falsy", () => {
+    const json = { and: ["ABCD123", "CDEF456"] }
+    const isRule = RequisiteJsonLogic.is_rule(json)
+    expect(isRule).toBe(false)
+  })
+
+  it("valid a simpler json, and with units + or, truthy", () => {
+    const json = {
+      and: [
+        {
+          units: 24,
+          from: [
+            {
+              subject: "CPSC",
+            },
+          ],
+        },
+        {
+          or: ["ACCT425", "ACCT357"],
+        },
+      ],
+    }
+    const isRule = RequisiteJsonLogic.is_rule(json)
+    expect(isRule).toBe(true)
+  })
+
+  it("valid a simpler json: and with units + units + or, falsy", () => {
+    const json = {
+      and: [
+        {
+          units: 24,
+          from: [
+            {
+              subject: "ABCD",
+            },
+          ],
+        },
+        {
+          units: 24,
+          from: [
+            {
+              department: "XXXXXX",
+            },
+          ],
+        },
+        {
+          or: [true, "ACCT301"],
+        },
+      ],
+    }
+    const isRule = RequisiteJsonLogic.is_rule(json)
+    expect(isRule).toBe(false)
+  })
+
+  it("valid a complex json: and with units + consent, truthy", () => {
+    const json = {
+      and: [
+        {
+          units: 15,
+          from: [
+            {
+              subject: "ART",
+              department: "ART",
+            },
+          ],
+        },
+        {
+          consent: {
+            faculty: "SC",
+          },
+        },
+      ],
+    }
+    const isRule = RequisiteJsonLogic.is_rule(json)
+    expect(isRule).toBe(true)
+  })
+
+  it("valid a year json", () => {
+    const json = {
+      year: "fourth",
+    }
+    const isRule = RequisiteJsonLogic.is_rule(json)
+    expect(isRule).toBe(true)
+  })
+
+  it("valid a complex json: and with course + admission + consent, truthy", () => {
+    const json = {
+      and: [
+        "KNES260",
+        {
+          admission: {
+            faculty: "KN",
+          },
+        },
+        {
+          consent: {
+            faculty: "KN",
+          },
+        },
+      ],
+    }
+    const isRule = RequisiteJsonLogic.is_rule(json)
+    expect(isRule).toBe(true)
+  })
+
+  it("valid a complex json: and with units + consent, truthy", () => {
+    const json = {
+      and: [
+        {
+          units: 18,
+          from: [
+            {
+              level: "300+",
+              subject: "ARHI",
+            },
+          ],
+        },
+        {
+          consent: {
+            department: "ART",
+          },
+        },
+      ],
+    }
+    const isRule = RequisiteJsonLogic.is_rule(json)
+    expect(isRule).toBe(true)
+  })
+
+  it("valid a complex json: and with units + nested or + year, truthy", () => {
+    const json = {
+      and: [
+        {
+          from: ["MATH249", "MATH265", "MATH275"],
+          units: 3,
+        },
+        {
+          or: [
+            {
+              units: 3,
+              from: [
+                {
+                  faculty: "SC",
+                },
+              ],
+            },
+            {
+              units: 3,
+              from: [
+                {
+                  faculty: "EN",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          year: "third",
+        },
+      ],
+    }
+    const isRule = RequisiteJsonLogic.is_rule(json)
+    expect(isRule).toBe(true)
+  })
+
+  it("valid a complex json: or with untracked course, truthy", () => {
+    const json = {
+      or: ["PLAN602", "Environmental Design Planning 602"],
+    }
+    const isRule = RequisiteJsonLogic.is_rule(json)
+    expect(isRule).toBe(true)
+  })
+
+  it("valid a complex json: units with level, falsy", () => {
+    const json = {
+      units: 3,
+      from: [
+        {
+          level: "AA",
+        },
+      ],
+    }
+    const isRule = RequisiteJsonLogic.is_rule(json)
+    expect(isRule).toBe(false)
+  })
+
+  it("validate course with topic number", () => {
+    const json = {
+      from: ["ENCI481", "ENEE377", "ENEE519.09"],
+      units: 3,
+    }
+    const isRule = RequisiteJsonLogic.is_rule(json)
+    expect(isRule).toBe(true)
   })
 })
